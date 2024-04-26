ddacf8020eced9648238704f685529d3b9dd6846
magniHomes <magniventures@proton.me>
2024-04-26 19:01:04 +0100

    chore: cleaning data and saving it to different formats

--- scrapy/bookscraper/bookscraper/pipelines.py
+++ scrapy/bookscraper/bookscraper/pipelines.py
@@ -6,22 +6,16 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
-
-
+from scrapy.exceptions import DropItem
 
 class BookscraperPipeline:
     def process_item(self, item, spider):
         adapter = ItemAdapter(item)
 
         # Strip all white spaces from strings
-        field_names = adapter.field_names()
-        for field_name in field_names:
-            if field_name != 'description':
-                value = adapter.get(field_name)
-                if isinstance(value, list):
-                    adapter[field_name] = [v.strip() for v in value]
-                else:
-                    adapter[field_name] = value.strip()
+        for field_name in adapter.field_names():
+            if isinstance(adapter[field_name], str):
+                adapter[field_name] = adapter[field_name].strip()
 
         # Category & Product Type --> switch to lowercase
         lowercase_keys = ['category', 'product_type']
@@ -30,8 +24,7 @@ class BookscraperPipeline:
             adapter[lowercase_key] = value.lower()
 
         # Price --> convert to float
-        price_keys = ['price', 'price_excl_tax', 'price_incl_tax', 'tax']
-        for price_key in price_keys:
+        for price_key in ['price', 'price_excl_tax', 'price_incl_tax', 'tax']:
             value = adapter.get(price_key)
             if value:  # Check if the value is not empty
                 value = value.replace('£', '')
@@ -43,34 +36,46 @@ class BookscraperPipeline:
                 adapter[price_key] = None
 
         # Availability --> extract number of books in stock
-        availability_string = adapter.get('availability')
+        availability_string = adapter.get('availability', '')
         split_string_array = availability_string.split('(')
         if len(split_string_array) < 2:
             adapter['availability'] = 0
         else:
-            availability_array = split_string_array[1].split(' ')
-            adapter['availability'] = int(availability_array[0])
+            availability_count = split_string_array[1].split(' ')
+            adapter['availability'] = int(availability_count[0])
 
-        # Reviews --> extract number of reviews
-        num_reviews_string = adapter.get('num_reviews')
-        adapter['num_reviews'] = int(num_reviews_string)
-
+        # Num Reviews --> extract number of reviews
+        num_reviews_string = adapter.get('num_reviews', '0')
+        adapter['num_reviews'] = self.clean_num_reviews(num_reviews_string)
 
         # Stars --> extract number of stars
-        stars_string = adapter.get('stars')
-        split_string_array = stars_string.split(' ')
-        stars_text_value = split_string_array[1].lower()
-        if stars_text_value == 'zero':
-            adapter['stars'] = 0
-        elif stars_text_value == 'one':
-            adapter['stars'] = 1
-        elif stars_text_value == 'two':
-            adapter['stars'] = 2
-        elif stars_text_value == 'three':
-            adapter['stars'] = 3
-        elif stars_text_value == 'four':
-            adapter['stars'] = 4
-        elif stars_text_value == 'five':
-            adapter['stars'] = 5
+        stars_string = adapter.get('stars', '')
+        adapter['stars'] = self.clean_stars(stars_string)
 
         return item
+
+    def clean_num_reviews(self, num_reviews_string):
+        # Extract number of reviews
+        try:
+            return int(num_reviews_string)
+        except ValueError:
+            return 0
+
+    def clean_stars(self, stars_string):
+        # Extract number of stars
+        split_string_array = stars_string.split(' ')
+        stars_text_value = split_string_array[1].lower() if len(split_string_array) > 1 else ''
+        if stars_text_value == 'zero':
+            return 0
+        elif stars_text_value == 'one':
+            return 1
+        elif stars_text_value == 'two':
+            return 2
+        elif stars_text_value == 'three':
+            return 3
+        elif stars_text_value == 'four':
+            return 4
+        elif stars_text_value == 'five':
+            return 5
+        else:
+            return 0
--- scrapy/bookscraper/bookscraper/spiders/bookspider.py
+++ scrapy/bookscraper/bookscraper/spiders/bookspider.py
@@ -7,6 +7,7 @@ class BookspiderSpider(Spider):
     start_urls = ["https://books.toscrape.com"]
 
     def parse(self, response):
+        # Extracting book URLs from the main page
         books = response.css('article.product_pod')
         for book in books:
             relative_url = book.css('h3 a ::attr(href)').get()
@@ -15,26 +16,38 @@ class BookspiderSpider(Spider):
             book_url = response.urljoin(relative_url)
             yield Request(book_url, callback=self.parse_book_page)
 
+        # Extracting the URL of the next page (if it exists)
         next_page = response.css('li.next a ::attr(href)').get()
         if next_page:
             next_page = response.urljoin('catalogue/' + next_page)
             yield Request(next_page, callback=self.parse)
 
     def parse_book_page(self, response):
+        # Initializing a BookItem object to store scraped data
         book_item = BookItem()
 
-        book_item['url'] = response.url,
-        book_item['title'] = response.css('.product_main h1::text').get(),
-        book_item['upc'] = table_rows[0].css("td ::text").get(),
-        book_item['product_type'] = response.css("table tr:nth-child(1) td::text").get(),
-        book_item['price_excl_tax'] = response.css("table tr:nth-child(2) td::text").get(),
-        book_item['price_incl_tax'] = response.css("table tr:nth-child(3) td::text").get(),
-        book_item['tax'] = response.css("table tr:nth-child(4) td::text").get(),
-        book_item['availability'] = response.css("table tr:nth-child(5) td::text").get(),
-        book_item['num_reviews'] = response.css("table tr:nth-child(6) td::text").get(),
-        book_item['stars'] = response.css("p.star-rating::attr(class)").get().split()[-1],
-        book_item['category'] = response.xpath("//ul[@class='breadcrumb']/li[@class='active']/preceding-sibling::li[1]/a/text()").get(),
-        book_item['description'] = response.xpath("//div[@id='product_description']/following-sibling::p/text()").get(),
-        book_item['price'] = response.css("p.price_color::text").get(),
+        # Extracting data from the book page
+        book_item['url'] = response.url
+        book_item['title'] = response.css('.product_main h1::text').get()
+        book_item['upc'] = response.xpath("//th[contains(text(),'UPC')]/following-sibling::td/text()").get()
+        book_item['product_type'] = response.xpath("//th[contains(text(),'Product Type')]/following-sibling::td/text()").get()
+        book_item['price_excl_tax'] = response.xpath("//th[contains(text(),'Price (excl. tax)')]/following-sibling::td/text()").get()
+        book_item['price_incl_tax'] = response.xpath("//th[contains(text(),'Price (incl. tax)')]/following-sibling::td/text()").get()
+        book_item['tax'] = response.xpath("//th[contains(text(),'Tax')]/following-sibling::td/text()").get()
+        book_item['availability'] = response.xpath("//th[contains(text(),'Availability')]/following-sibling::td/text()").get()
+        book_item['num_reviews'] = response.xpath("//th[contains(text(),'Number of reviews')]/following-sibling::td/text()").get()
 
-        yield book_item
+        # Extracting star rating (e.g., five-star, four-star, etc.)
+        book_item['stars'] = response.css("p.star-rating::attr(class)").get().split()[-1]
+
+        # Extracting the category of the book
+        book_item['category'] = response.xpath("//ul[@class='breadcrumb']/li[@class='active']/preceding-sibling::li[1]/a/text()").get()
+
+        # Extracting the book description
+        book_item['description'] = response.xpath("//div[@id='product_description']/following-sibling::p/text()").get()
+
+        # Extracting the price (without tax)
+        book_item['price'] = response.css("p.price_color::text").get()
+
+        # Returning the BookItem object
+        return book_item
