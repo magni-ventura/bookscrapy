b4e8e721da8d95c39ae6474085a8db0ec065afd1
magniHomes <magniventures@proton.me>
2024-04-26 15:56:40 +0100

    Chore: added and declared inquires in items.py for con currency

--- scrapy/bookscraper/bookscraper/spiders/bookspider.py
+++ scrapy/bookscraper/bookscraper/spiders/bookspider.py
@@ -1,21 +1,39 @@
-import scrapy
- 
+from scrapy import Spider, Request
+from bookscraper.items import BookItem
 
-class BookspiderSpider(scrapy.Spider):
+class BookspiderSpider(Spider):
     name = "bookspider"
     allowed_domains = ["books.toscrape.com"]
     start_urls = ["https://books.toscrape.com"]
 
     def parse(self, response):
         books = response.css('article.product_pod')
-
         for book in books:
-            next_page = response.css('li.next a ::attr(href)').get()
+            relative_url = book.css('h3 a ::attr(href)').get()
+            if 'catalogue/' not in relative_url:
+                relative_url = 'catalogue/' + relative_url
+            book_url = response.urljoin(relative_url)
+            yield Request(book_url, callback=self.parse_book_page)
 
-            #if the catalogue/ is in the next page
-            if 'catalogue/' in next_page:
-                next_page_url = 'https://books.toscrape.com/' + next_page
-            else:
-                # if not in the next page it should be added and ran
-                next_page_url = 'https://books.toscrape.com/catalogue/' + next_page
-            yield response.follow(next_page_url, callback= self.parse)
+        next_page = response.css('li.next a ::attr(href)').get()
+        if next_page:
+            next_page = response.urljoin('catalogue/' + next_page)
+            yield Request(next_page, callback=self.parse)
+
+    def parse_book_page(self, response):
+        book_item = BookItem()
+
+        book_item['url'] = response.url
+        book_item['title'] = response.css('.product_main h1::text').get()
+        book_item['product_type'] = response.css("table tr:nth-child(1) td::text").get()
+        book_item['price_excl_tax'] = response.css("table tr:nth-child(2) td::text").get()
+        book_item['price_incl_tax'] = response.css("table tr:nth-child(3) td::text").get()
+        book_item['tax'] = response.css("table tr:nth-child(4) td::text").get()
+        book_item['availability'] = response.css("table tr:nth-child(5) td::text").get()
+        book_item['num_reviews'] = response.css("table tr:nth-child(6) td::text").get()
+        book_item['stars'] = response.css("p.star-rating::attr(class)").get().split()[-1]
+        book_item['category'] = response.xpath("//ul[@class='breadcrumb']/li[@class='active']/preceding-sibling::li[1]/a/text()").get()
+        book_item['description'] = response.xpath("//div[@id='product_description']/following-sibling::p/text()").get()
+        book_item['price'] = response.css("p.price_color::text").get()
+
+        yield book_item
--- scrapy/bookscraper/bookscraper/spiders/train1.py
+++ scrapy/bookscraper/bookscraper/spiders/train1.py
@@ -1,4 +1,5 @@
-"""import scrapy
+"""
+import scrapy
 
 
 class BookspiderSpider(scrapy.Spider):
@@ -25,4 +26,82 @@ class BookspiderSpider(scrapy.Spider):
             else:
                 # if not in the next page it should be added and ran
                 next_page_url = 'https://books.toscrape.com/catalogue/' + next_page
-            yield response.follow(next_page_url, callback= self.parse)"""
+            yield response.follow(next_page_url, callback= self.parse)
+
+"""
+
+
+
+
+"""
+
+import scrapy
+from bookscraper.items import BookItem
+
+class BookspiderSpider(scrapy.Spider):
+    name = "bookspider"
+    #To control the spider
+    allowed_domains = ["books.toscrape.com"]
+    #Multiple urls can be added to this section
+    start_urls = ["https://books.toscrape.com"]
+
+    def parse(self, response):
+        #the Item in search of
+        books = response.css('article.product_pod')
+
+        #loop through the books
+        for book in books:
+            relative_url = response.css('h3 a ::attr(href)').get()
+
+            #if the catalogue/ is in the next page
+            if 'catalogue/' in relative_url:
+                book_url = 'https://books.toscrape.com/' + relative_url
+            else:
+                # if not in the next page it should be added and ran
+                book_url = 'https://books.toscrape.com/catalogue/' + relative_url
+            yield response.follow( book_url, callback= self.parse_book_page)
+
+            next_page = response.css('li.next a ::attr(href)').get()
+
+            if next_page is not None:
+                if 'catalogue' in next_page:
+                    next_page_url = 'https://books.toscrape.com/' + next_page
+                else:
+                    next_page_url = 'https://books.toscrape.com/catalogue/' + next_page
+                yield response.follow(next_page_url, callback= self.parse)
+
+    def parse_book_page(self, response):
+
+        table_rows = response.css("table tr")
+        book_item = BookItem()
+
+
+        BookItem['url'] = response.url,
+
+        BookItem['title'] = response.css('.product_main h1::text').get(),
+
+        BookItem['product_type'] = table_rows[1].css('td ::text').get(),
+
+        BookItem['price_excl_tax'] = table_rows[2].css('td ::text').get(),
+
+        BookItem['price_incl_tax'] = table_rows[3].css('td ::text').get(),
+
+        BookItem['tax'] = table_rows[4].css('td ::text').get(),
+
+        BookItem['availability'] = table_rows[5].css('td ::text').get(),
+
+        BookItem['num_reviews'] = table_rows[6].css('td ::text').get(),
+
+        BookItem['stars'] = response.css("p.star-rating").attrib['class'],
+
+        BookItem['category'] = response.xpath("//ul[@class='breadcrumb']/li[@class='active']/preceding-sibling::li[1]/a/text()").get(),
+
+        BookItem['description']= response.xpath("//div[@id='product_description']/following-sibling::p/text()").get(),
+
+        BookItem['price'] = response.css('p.price_color ::text()').get(),
+
+
+        yield BookItem
+
+
+"""
